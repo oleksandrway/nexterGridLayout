8dc7c7e795512d975d4065f95aec78046f92c595
Oleksandr Kryvoruchko <oleksandrkryvoruchko2000@gmail.com>
2022-08-16 15:37:08 +0300

    made a sidebar__menu

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
 import './src/sass/main.scss'
+
+const menu = document.querySelector('.sidebar__menu')
+const menuSwitcher = document.querySelector('.sidebar__menu-switcher')
+const html = document.querySelector('html')
+
+menuSwitcher.addEventListener('click', () => {
+  menu.classList.toggle('sidebar__menu-active')
+  menuSwitcher.classList.toggle('sidebar__nav-btn-active')
+  html.classList.toggle('noScroll')
+})
+
+menu.addEventListener('click', (e) => {
+  if (e.target === menu) {
+    menu.classList.toggle('sidebar__menu-active')
+    menuSwitcher.classList.toggle('sidebar__nav-btn-active')
+    html.classList.toggle('noScroll')
+  }
+})
