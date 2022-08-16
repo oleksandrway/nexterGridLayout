b274026c161d572eade04ce14a29c1516165c5d8
Oleksandr Kryvoruchko <oleksandrkryvoruchko2000@gmail.com>
2022-08-16 16:32:30 +0300

    added hover effects
     to header__seenon-logos
    to galleries__img
    to home cards

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import './src/sass/main.scss'
 
 const menu = document.querySelector('.sidebar__menu')
 const menuSwitcher = document.querySelector('.sidebar__menu-switcher')
+const menuItems = document.querySelectorAll('.sidebar__menu-item')
 const html = document.querySelector('html')
 
 menuSwitcher.addEventListener('click', () => {
@@ -12,6 +13,22 @@ menuSwitcher.addEventListener('click', () => {
 
 menu.addEventListener('click', (e) => {
   if (e.target === menu) {
+    menu.classList.toggle('sidebar__menu-active')
+    menuSwitcher.classList.toggle('sidebar__nav-btn-active')
+    html.classList.toggle('noScroll')
+  }
+})
+
+menuItems.forEach((item) => {
+  item.addEventListener('click', () => {
+    menu.classList.toggle('sidebar__menu-active')
+    menuSwitcher.classList.toggle('sidebar__nav-btn-active')
+    html.classList.toggle('noScroll')
+  })
+})
+
+document.addEventListener('keydown', (e) => {
+  if (e.code == 'Escape' && menu.classList.contains('sidebar__menu-active')) {
     menu.classList.toggle('sidebar__menu-active')
     menuSwitcher.classList.toggle('sidebar__nav-btn-active')
     html.classList.toggle('noScroll')
